db72b0c1a7e55ba05149b0004f15257be3412df3
yunqian44 <yunqian8@live.com>
2020-10-10 17:15:53 +0800

    新增用户实体

---
+++ Azure.CosmosDB/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.CosmosDB.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public string Remark { get; set; }
+    }
+}

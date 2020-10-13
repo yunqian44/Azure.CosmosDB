f03be6a6bd3987ea92a82cf12c2ae6143039757d
yunqian44 <yunqian8@live.com>
2020-10-13 18:50:21 +0800

    add UserController and Index.cshtml

---
+++ Azure.CosmosDB/Controllers/UserController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.CosmosDB.Models;
+using Azure.CosmosDB.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Azure.CosmosDB.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        private readonly IUserService _userService;
+
+        private IMemoryCache _cache;
+
+        public UserController(IUserService userService, IMemoryCache cache)
+        {
+            _userService = userService;
+            _cache = cache;
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(_userService.GetAll());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserViewModel userViewModel)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                //_cache.Remove("ErrorData");
+                //ViewBag.ErrorData = null;
+                // 视图模型验证
+                if (!ModelState.IsValid)
+                    return View(userViewModel);
+
+
+                // 执行添加方法
+                _userService.Register(userViewModel);
+                ViewBag.Sucesso = "用户注册成功！";
+
+                return View(userViewModel);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

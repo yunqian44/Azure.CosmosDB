08360b8f8ae602674c876a97925445b4044660ec
yunqian44 <yunqian8@live.com>
2020-10-13 18:52:18 +0800

    add userViewModel

---
+++ Azure.CosmosDB/Models/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.CosmosDB.Models
+{
+    public class UserViewModel
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Age
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Address
+        /// </summary>
+        public string Address { get; set; }
+    }
+}

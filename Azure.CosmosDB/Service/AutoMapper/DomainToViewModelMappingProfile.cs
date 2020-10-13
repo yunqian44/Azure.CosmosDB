87bb5255ebb39b6d8ee3cfaa1dafc2dac1c5d242
yunqian44 <yunqian8@live.com>
2020-10-13 18:49:39 +0800

    automap configuration

---
+++ Azure.CosmosDB/Service/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Azure.CosmosDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.CosmosDB.Service.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        /// <summary>
+        /// 配置构造函数，用来创建关系映射
+        /// </summary>
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<UserModel, UserViewModel>();
+        }
+    }
+}
---
+++ Azure.CosmosDB/Service/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Azure.CosmosDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.CosmosDB.Service.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            //手动进行配置
+            CreateMap<UserViewModel, UserModel>();
+        }
+    }
+}

d357f83fe330a8890191f4e2819edc5cf33cd0a5
yunqian44 <yunqian8@live.com>
2020-10-14 18:47:28 +0800

    add PartitionKey

--- Azure.CosmosDB/Models/UserModel.cs
+++ Azure.CosmosDB/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace Azure.CosmosDB.Models
 {
     public class UserModel
     {
+        [Key]
+        public string PartitionKey { get; set; }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
--- Azure.CosmosDB/Models/UserViewModel.cs
+++ Azure.CosmosDB/Models/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,23 +9,32 @@ namespace Azure.CosmosDB.Models
     public class UserViewModel
     {
         /// <summary>
-        /// Id
+        /// PartitionKey
         /// </summary>
+        public string PartitionKey { get; set; }
+
+        /// <summary>
+        /// No
+        /// </summary>
+        [DisplayName("No")]
         public int Id { get; set; }
 
         /// <summary>
         /// Name
         /// </summary>
+        [DisplayName("Name")]
         public string Name { get; set; }
 
         /// <summary>
         /// Age
         /// </summary>
+        [DisplayName("Age")]
         public int Age { get; set; }
 
         /// <summary>
         /// Address
         /// </summary>
+        [DisplayName("Address")]
         public string Address { get; set; }
     }
 }

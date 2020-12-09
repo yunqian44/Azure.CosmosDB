8fbce8b6d9bfab4763474fbd5f7da9df3c2da7b5
yunqian44 <yunqian8@live.com>
2020-12-09 16:26:19 +0800

    Implement the Cosmos DB CURD method

--- Azure.CosmosDB.ConsoleDemo/Program.cs
+++ Azure.CosmosDB.ConsoleDemo/Program.cs
@@ -3,6 +3,8 @@ using Azure.CosmosDB.ConsoleDemo.Model;
 using Microsoft.Azure.Cosmos;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Azure.CosmosDB.ConsoleDemo
@@ -24,47 +26,74 @@ namespace Azure.CosmosDB.ConsoleDemo
             Program p = new Program();
             await p.InitCosmosDB();
             await p.InitItem();
-            await p.QueryItems();
+
+            await p.QueryItems(string.Empty);
+
+            await p.ModifyItem("令狐冲");
+
+
+            await p.DeleteItem("令狐冲");
+
+
+            await p.QueryItems(string.Empty);
+
+
         }
 
-        public async List<UserModel> QueryItems()
+        /// <summary>
+        /// Query user base on name 
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <returns></returns>
+        public async Task<List<UserModel>> QueryItems(string name)
         {
-            var sqlQueryText = "SELECT * FROM c WHERE c.LastName = 'Andersen'";
+            var sqlQueryText = "SELECT * FROM c where 1=1";
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                sqlQueryText += " and c.Name='" + name + "'";
+            }
 
             Console.WriteLine("Running query: {0}\n", sqlQueryText);
 
-
             QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
-            
-            List<UserModel> families = new List<UserModel>();
+
+            var users = new List<UserModel>();
+            var queryUsers = this.container.GetItemQueryIterator<UserModel>(queryDefinition);
 
 
-            var users = this.container.GetItemQueryIterator<UserModel>(queryDefinition);
-            foreach (User user in users)
+            while (queryUsers.HasMoreResults)
             {
-                yield return user;
+                FeedResponse<UserModel> currentResultSet = await queryUsers.ReadNextAsync();
+                foreach (UserModel user in currentResultSet)
+                {
+                    Console.WriteLine("\tRead {0}\n", user.Name);
+                    users.Add(user);
+                }
             }
-                
-            
+            return users;
         }
-
 
 
         public async Task InitCosmosDB()
         {
             var a = new Appsettings();
 
-            this.cosmosClient = new CosmosClient(Appsettings.app("CosmosDB", "Endpoint"), Appsettings.app("CosmosDB", "Key"));
+            #region Create CosmosClient
+            this.cosmosClient = new CosmosClient(Appsettings.app("CosmosDB", "Endpoint"), Appsettings.app("CosmosDB", "Key")); 
+            #endregion
 
             #region Create CosmosDB
-            this.database= await this.cosmosClient.CreateDatabaseIfNotExistsAsync(Appsettings.app("CosmosDB", "DataBase"));
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(Appsettings.app("CosmosDB", "DataBase"));
             Console.WriteLine("Created Database:{0} Success\n", Appsettings.app("CosmosDB", "DataBase"));
             #endregion
 
-            this.container = await this.database.CreateContainerIfNotExistsAsync(Appsettings.app("CosmosDB", "Container"), "/id");
+            #region Create Container 
+            this.container = await this.database.CreateContainerIfNotExistsAsync(Appsettings.app("CosmosDB", "Container"), "/id"); 
+            #endregion
+
             Console.WriteLine("Created Container:{0} Success\n", Appsettings.app("CosmosDB", "Container"));
         }
-
 
         public async Task InitItem()
         {
@@ -78,28 +107,67 @@ namespace Azure.CosmosDB.ConsoleDemo
                 Remark = "中央音乐学院"
             };
 
-            var user2 = new UserModel { 
-                Id = "2", 
-                Name = "令狐冲", 
-                Age = 20, 
-                Address = "佛山市南海区灯湖东路6号", 
-                Remark = "广发商学院" 
+            var user2 = new UserModel
+            {
+                Id = "2",
+                Name = "令狐冲",
+                Age = 20,
+                Address = "佛山市南海区灯湖东路6号",
+                Remark = "广发商学院"
             };
+
+            #region Query User1 of '张无忌'
+            var user1s = await QueryItems(user1.Name);
+            #endregion
 
 
             #region Add User1 Item
-            ItemResponse<UserModel> user1Response = await this.container.CreateItemAsync<UserModel>(user1, new PartitionKey(user1.Id));
+            if (user1s.Count <= 0)
+            {
+                ItemResponse<UserModel> user1Response = await this.container.CreateItemAsync<UserModel>(user1, new PartitionKey(user1.Id));
 
-            Console.WriteLine("Created Item in database with id:{0} Operation consumed {1} RUs.\n", user1Response.Resource.Id, user1Response.StatusCode);
+                Console.WriteLine("Created Item in database with id:{0} Operation consumed {1} RUs.\n", user1Response.Resource.Id, user1Response.StatusCode);
+            }
+            #endregion
+
+
+            #region Query User2 of '令狐冲'
+            var user2s = await QueryItems(user2.Name);
             #endregion
 
             #region Add User2 Item
-            ItemResponse<UserModel> user2Response = await this.container.CreateItemAsync<UserModel>(user2, new PartitionKey(user2.Id));
+            if (user2s.Count <= 0)
+            {
+                ItemResponse<UserModel> user2Response = await this.container.CreateItemAsync<UserModel>(user2, new PartitionKey(user2.Id));
 
-            Console.WriteLine("Created Item in database with id:{0} Operation consumed {1} RUs.\n", user2Response.Resource.Id, user1Response.StatusCode);
+                Console.WriteLine("Created Item in database with id:{0} Operation consumed {1} RUs.\n", user2Response.Resource.Id, user2Response.StatusCode);
+            }
             #endregion
         }
 
+        /// <summary>
+        /// Delete user base on name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task DeleteItem(string name)
+        {
+            var users= await QueryItems(name);
+
+           var deleteResponse= await this.container.DeleteItemAsync<UserModel>(users.First().Id, new PartitionKey(users.First().Id));
+
+            Console.WriteLine("delete'" + name + " item'{0}\n", deleteResponse.StatusCode == HttpStatusCode.NoContent ? "success" : "fail");
+        }
+
+        public async Task ModifyItem(string name)
+        {
+            var users = await QueryItems(name);
+
+            var modifyUser = users.FirstOrDefault();
+            modifyUser.Address = "上海市静安区石板街73弄";
+           var modifyResponse= await this.container.ReplaceItemAsync<UserModel>(modifyUser, modifyUser.Id, new PartitionKey(modifyUser.Id));
+            Console.WriteLine("name equal to '"+name+"',his family address modify {0}\n", modifyResponse.StatusCode==HttpStatusCode.OK?"success":"fail");
+        }
 
     }
 }
